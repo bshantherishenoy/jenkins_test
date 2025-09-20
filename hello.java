9c0a922c5cffcacfcc15d2de8e98a075ba188e70
Shantheri shenoy <63908336+bshantherishenoy@users.noreply.github.com>
2025-09-20 07:43:58 +0000

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 public static void main(String args[]){
-System.out.println("Hello world, this is Deepak Bhai's course");
+System.out.println("Hello world, this is Deepak Bhai's course.");
 
 }
 }
